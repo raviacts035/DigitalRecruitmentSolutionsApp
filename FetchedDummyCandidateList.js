f23f04a0f9c9dd1ba65bc54615c3b641f63e1694
raviacts035 <ravindrainturi035@gmail.com>
2023-07-09 13:38:30 +0530

    candidates, search funtionality

---
+++ FetchedDummyCandidateList.js
@@ -0,0 +1,50 @@
+const fetched_candidates=[
+    {
+        name:"Doremon",
+        experience:5,
+        role:'UI developer',
+        city:'Jaipur',
+        photo_url:'./assets/recruiter_dp1.jpg',
+        age:33
+    },
+    {
+        name:"Kumar",
+        experience:2,
+        role:'java developer',
+        city:'pune',
+        photo_url:'./assets/recruiter_dp3.jpg',
+        age:27
+    },
+    {
+        name:"Nelson",
+        experience:10,
+        role:'UX designer',
+        city:'Jaipur',
+        photo_url:'./assets/recruiter_dp1.jpg',
+        age:38
+    },
+    {
+        name:"Bhanu",
+        experience:7,
+        role:'Account Manager',
+        city:'chenni',
+        photo_url:'./assets/recruiter_dp1.jpg',
+        age:32
+    },
+    {
+        name:"Doremon",
+        experience:5,
+        role:'UI developer',
+        city:'Lucknow',
+        photo_url:'./assets/recruiter_dp1.jpg',
+        age:24
+    },
+    {
+        name:"terisa",
+        experience:3,
+        role:'IOS developer',
+        city:'bangolre',
+        photo_url:'./assets/recruiter_dp1.jpg',
+        age:24
+    }
+]
--- script.js
+++ script.js
@@ -21,3 +21,53 @@ async function handleSignup(event){
     if (responce.ok) window.location.href = './dashbord.html'
 
 }
+
+
+// initiating filterred candidates default list
+var filtered_candidates=fetched_candidates;
+
+
+// candidate search function
+function handleSearch(){
+  var searchText=document.querySelector('#search').value
+  if (!searchText){
+    filtered_candidates=fetched_candidates;
+    appendCandidates();
+  }
+  let result=fetched_candidates.filter((element)=>{
+      let title=element.role.toLowerCase().search(searchText.toLowerCase())
+      let city=element.city.toLowerCase().search(searchText.toLowerCase())
+      return title+1 || city+1
+  })
+  filtered_candidates= [...result]
+  appendCandidates();
+}
+
+
+const canidate_pool_cont=document.querySelector('#canidate_pool')
+function appendCandidates(){
+    var allCards=''
+    filtered_candidates.map(candidate=>{
+        const card=`
+        <article class="cand_card_margin candidate_card2">
+        <div class="flex_cont ">
+          <div class="profile_img_cont2">
+            <img class="profile_img1" src="${candidate.photo_url}" alt="candidate" />
+          </div>
+          <div class="person_name">
+            <h3>${candidate.name}</h3>
+            <p>${candidate.role} - ${candidate.experience} years</p>
+          </div>
+        </div>
+        <hr class="line_margin">
+        <div class="flex_cont flex_between">
+          <p>Location : ${candidate.city}</p>
+          <p>Age : ${candidate.age}</p>
+        </div>
+        </article>`
+        allCards+=card
+    })
+    canidate_pool_cont.innerHTML=allCards
+    return
+}
+appendCandidates();
